1cdb63a96ec7343f16efb2b95d3e0b84b50c0aa5
Sandro <sandrocoelho2023a@gmail.com>
2026-05-20 07:04:36 -0300

    instalando bibliotecas inciais e criando index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// importando bibliotecas
+const express = require('express');
+const cors = require('cors');
+
+// definições iniciais
+const app = express();
+const porta = 5000;
+
+// middlewares
+app.use(express.json());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+// rotas
+
+// escuta de porta
+app.listen(porta, () => {
+    console.log(`O express está escutando a porta ${porta}`);
+})
+
