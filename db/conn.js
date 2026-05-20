ca8645c7ee5b69caf035763b747d88d0e95e13f0
Sandro <sandrocoelho2023a@gmail.com>
2026-05-20 07:10:39 -0300

    criando conexão com banco de dados mongo.db

---
+++ db/conn.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+async function main() {
+    const conn = mongoose.connect('mongodb://localhost:27017/gestaoFrotaTrens');
+    console.log("Banco de dados conectado com sucesso!");
+
+}
+
+main().catch((err) => {
+    console.log(err);
+    return;
+})
+
+module.exports = mongoose;
